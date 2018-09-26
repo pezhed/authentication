3f37ccab16a01908d815ac2d22ad331906772c45
Eric Lopez <ericlopez07@gmail.com>
2018-09-26 05:05:54 -0600

    Added firebase connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common/Index';
+import firebase from 'firebase';
 
 
 class App extends Component {
+
+  componentWillMount() {
+    
+    firebase.initializeApp({
+      apiKey: "AIzaSyDgKnjfYO3r3oiCYY2_SgxKz7meEAPeXNI",
+      authDomain: "authentication-47cd7.firebaseapp.com",
+      databaseURL: "https://authentication-47cd7.firebaseio.com",
+      projectId: "authentication-47cd7",
+      storageBucket: "authentication-47cd7.appspot.com",
+      messagingSenderId: "758509034986"
+    });
+  }
+
   render() {
     return (
       <View>
